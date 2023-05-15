95605692899543f637ddd1b296fd1614091345a5
Se Tech Hive <se_tech_hive@outlook.com>
2023-05-16 05:13:46 +0800

    Added meaningful comments

--- backend/controller/bankController.js
+++ backend/controller/bankController.js
@@ -1,22 +1,42 @@
 import { findOne } from '../models/bankModel';
 
+    /**
+     * ? Check the balance of the account based on the account number given
+     * ! If account number did not exist return 404
+     * * It will return to 200 and the balance of the account.
+     * ! If it catched any unknown error return to 500
+     * @param req the object for request
+     * @param res the object for response
+     */  
 const checkBalance = async (req, res) => {
     try {
+
       const { accountNumber } = req.params
-      // Retrieve the account information from the database or some other source
       const accountInfo = await findOne({ accountNumber })
+
+      // if account not found
       if (!accountInfo) {
-        // If no account information is found, return a 404 Not Found error
         return res.status(404).json({ error: 'Account not found' })
       }
-      // If the account information is found, return it as a JSON object
+
+      // if found
       res.status(200).json( {balance : accountInfo.balance })
+
     } catch (err) {
-      // If there is an error, return a 500 Internal Server Error
+      // Unknown error
       return res.status(500).json({ error: 'Internal server error' })
     }
 }
 
+    /**
+     * ? This will just add the value of what is in the account to whatever the amount is being deposited
+     * ! If account number did not exist return 404
+     * * It will return to 200 and the balance of the account and save.
+     * ! If it catched any unknown error return to 500
+     * @param req the object for request
+     * @param res the object for response
+     */  
+    
 const deposit = async (req, res) => {
     try {
         const { accountNumber, depositAmount } = req.body
@@ -39,6 +59,16 @@ const deposit = async (req, res) => {
         return res.status(500).json({ error: 'Internal server error' })
     }
 }
+
+    /**
+     * ? This will just subtract the value of what is in the account to whatever the amount is being withdrawn
+     * ! If account number did not exist return 404
+     * ! If the account has insufficient balance or the amount being withdrawn is greater then it will return to 400
+     * * It will return to 200 and the balance of the account and save.
+     * ! If it catched any unknown error return to 500
+     * @param req the object for request
+     * @param res the object for response
+     */  
 
 const withdraw = async (req, res) => {
     try {
@@ -68,6 +98,16 @@ const withdraw = async (req, res) => {
     }
 }
 
+    /**
+     * ? This will just close an existing account
+     * ! If account number did not exist return 404
+     * ! If account is already closed it will just return 400 and an error message
+     * * It will return to 200 and the status of the account (0 for closed and 1 for active).
+     * ! If it catched any unknown error return to 500
+     * @param req the object for request
+     * @param res the object for response
+     */  
+
 const closeAccount = async(req, res) => {
     try {
         const { accountNumber } = req.body
@@ -79,11 +119,18 @@ const closeAccount = async(req, res) => {
             return res.status(404).json({ error: 'Account not found' })
         }
 
+        // If already closed
+        if(account.status === 0) {
+            return res.status(400).json( {error: 'Account is already closed'} )
+        }
+
         account.status = 0
+        await account.save()
 
         // return updated status
         res.status(200).json({ status: account.status })
     }catch(err) {
+        // Unknown error
         return res.status(500).json({ error: 'Internal server error' })
     }
 }
