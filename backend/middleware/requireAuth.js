35953a03afebf8d3d6b7cafcf4da3adee05382b2
Se Tech Hive <se_tech_hive@outlook.com>
2023-06-14 02:30:11 +0800

    Consistency check

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -4,20 +4,20 @@ const User = require('../models/userModel')
 const requireAuth = async (req, res, next) => {
     try {
       // Verify authentication
-      const { authorization } = req.headers;
+      const { authorization } = req.headers
   
       if (!authorization) {
-        return res.status(401).json({ error: 'Auth required' });
+        return res.status(401).json({ error: 'Authorization Required' })
       }
   
-      const token = authorization.split(' ')[1];
+      const token = authorization.split(' ')[1]
   
-      const { _id } = jwt.verify(token, process.env.SECRET);
+      const { _id } = jwt.verify(token, process.env.SECRET)
   
-      req.user = await User.findOne({ _id }).select('_id');
-      next();
+      req.user = await User.findOne({ _id }).select('_id')
+      next()
     } catch (err) {
-      res.status(401).json({ error: 'Request is not authorized' });
+      res.status(401).json({ error: 'You are not authorized to do that' })
     }
 };
   
--- backend/models/profileModel.js
+++ backend/models/profileModel.js
@@ -17,7 +17,7 @@ const profileSchema = new mongoose.Schema({
         type: String,
         required: false
     },
-    profilePicture: {
+    picture: {
         type: String,
         required: false
     },
