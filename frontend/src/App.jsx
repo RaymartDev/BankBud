ae1ea0c73f4838ffb3dab5cfa50564c0e0b451ba
Se Tech Hive <se_tech_hive@outlook.com>
2023-06-14 03:15:26 +0800

    Added layout for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
-function App() {
+import Pages from './pages'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext';
+
+const App = () => {
+  const { user } = useAuthContext()
   
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route 
+          path='/'
+          element={user ? <Pages.Home /> : <Pages.Landing />} />
+        <Route 
+          path='/login'
+          element={user ? <Navigate to="/" /> : <Pages.Login />} />
+        <Route 
+          path='/register'
+          element={user ? <Navigate to="/" /> : <Pages.Register />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,33 @@
+const Landing = () => {
+    return (
+        <>
+            <nav>
+                
+            </nav>
+
+            <main>
+                <section className="wall">
+                    
+                </section>
+
+                <section className="goals">
+
+                </section>
+
+                <section className="download">
+
+                </section>
+
+                <section className="start">
+
+                </section>
+            </main>
+
+            <footer>
+
+            </footer>
+        </>
+    )
+}
+
+export default Landing
