38ac588b655a520465b850e5b6f301d87bbb9306
Leanseln <oleanselnico@gmail.com>
2023-06-30 19:07:08 +0800

    Login and Register

---
+++ frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BankBud from '../../assets/images/BankBudLogo/LogoName.png'
+
+const navbar = () => {
+  return (
+    <nav>
+        <img src={BankBud}/>
+    </nav>
+  )
+}
+
+export default navbar
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,49 @@
+import Navbar from '../components/navbar/navbar'
+import Footer from '../components/footer/footer'
+
+
 const Register = () => {
     return (
-        <>Register</>
+        <div className="register">
+            <Navbar />
+            <div className="registerSection">
+                <form className='registerForm'>
+                    <div className='registerDetails'>
+                        <p>Set up your Account</p>
+                        
+                        <hr />
+
+                        <input 
+                        type='string'
+                        required='true'
+                        placeholder='Enter your Full name'
+                        />
+
+                        <input 
+                        type='string'
+                        required='true'
+                        placeholder='Enter your Email'
+                        />
+
+                        <input 
+                        type='string'
+                        required='true'
+                        placeholder='Enter your Password'
+                        />
+
+                        <input 
+                        type='string'
+                        required='true'
+                        placeholder='Confirm your Password'
+                        />
+
+                        <button className='registerBtn'>Create Account</button>
+                    </div>
+
+                </form>
+            </div>
+            <Footer />
+        </div>
     )
 }
 
